26a5c6037b6224ba0c3ca28dc4f3f00645a3d25b
hingyimartin <hingyimartin@gmail.com>
2025-05-26 21:30:44 +0200

    Init repo

---
+++ broker-and-connector/server/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.listen(process.env.SERVER_PORT || 5000, () => {
+  console.log(`Server running on port: ${process.env.SERVER_PORT || 5000}`);
+});
